72757d3ead9fbac7a87244e054356ef0a3c29bf7
Zhu Ran <zhuran94@163.com>
2016-06-19 19:23:19 +0800

    [Mengqiu] add react and react-dom into this project and use in as the first time.

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+const React = require('react');
+const {render} = require('react-dom');
+
+const App = React.createClass({
+    render (){
+        return (
+            <div>
+                Hello, world!
+            </div>
+        );
+    }
+});
+
+render(<App/>, document.getElementById('app'));
