61b27580dc9f40e9c9b369511ee64ed7e1dc6396
Zhu Ran <zhuran94@163.com>
2016-06-21 12:10:35 +0800

    [mengqiu] 3 important redux methods.

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,6 +1,4 @@
-
 import { createStore } from 'redux';
-import {render} from 'react-dom';
 
 const counter = (state = 0, action)=>{
     switch (action.type) {
@@ -13,28 +11,14 @@ const counter = (state = 0, action)=>{
     }
 };
 
-const Counter = ({value, onIncrement, onDecrement}) => (
-    <div>
-        <h1>{value}</h1>
-        <button onClick={onIncrement}>+</button>
-        <button onClick={onDecrement}>-</button>
-    </div>
+const store = createStore(counter);
 
-);
+ store.subscribe(()=>{
+    document.body.innerText = store.getState();
+ });
 
-const renderApp = ()=>{
-  render(
-    <Counter
-        value = {store.getState()}
-        onIncrement = {()=> store.dispatch({type: 'INCREMENT'})}
-        onDecrement = {()=> store.dispatch({type: 'DECREMENT'})}
-        />,
-     document.getElementById('app')
-  );
-
-}
-
-const store = createStore(counter); // When you create a store, you need to specify a 'reducer' to tell store how to manage state.
-store.subscribe(renderApp);
+document.addEventListener('click', ()=>{
+    store.dispatch({type: 'INCREMENT'});
+});
 
 
